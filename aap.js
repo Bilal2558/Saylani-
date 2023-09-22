6f1b2ed2cfabb96e7cbb46f27ca0fab4b2876262
Bilal2558 <137030303+Bilal2558@users.noreply.github.com>
2023-09-22 18:23:25 +0500

    js

---
+++ aap.js
@@ -0,0 +1,15 @@
+// // 1
+
+// alert("error please enter a valid password")
+
+// // 2
+
+// var name = "Abc"
+
+// alert(name)
+
+// 3
+var linebreak = "welcome to js land...
+Happy coding"
+
+alert(linebreak)
